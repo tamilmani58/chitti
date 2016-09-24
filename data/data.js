662baeb51b7138f4264a35e2bacc7a8d11ffc5b1
abhishek.sin <abhishek.sin@directi.com>
2016-09-24 16:41:55 +0530

    receive upload changes

---
+++ data/data.js
@@ -0,0 +1,19 @@
+var userData = [
+  {
+      "flockId": "4llt4rm20lcleer2",
+      "userAd": "tamil.s",
+      "email": "tamil.s@media.net"
+  },
+    {
+        "flockId": "a6t3xtxxt33tixvv",
+        "userAd": "pritam.p",
+        "email": "pritam.p@media.net"
+    },
+    {
+        "flockId": "ws4hd4pq0cypdcdy",
+        "userAd": "abhishek.sin",
+        "email": "abhishek.sin@media.net"
+    }
+];
+
+module.exports = userData;
--- services/outGoingHookService.js
+++ services/outGoingHookService.js
@@ -1,6 +1,8 @@
+var userRepository = require("../repositories/userRepository");
 function receiveMessage(req, res){
     console.log(req.body);
-    res.send("Got the message");
+
+    res.send("Got the Message");
 }
 
 module.exports = receiveMessage;
