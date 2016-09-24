6538a89f5b94bdb79e3cc6841e1350b8bf9ab95d
abhishek.sin <abhishek.sin@directi.com>
2016-09-24 17:47:38 +0530

    botservice

--- config/appconfig.js
+++ config/appconfig.js
@@ -1,9 +1,11 @@
 var config = {
-    DEFAULT_GROUP_ID: "2a9f0874-bf99-4636-9376-0a34e5c09029",
+    DEFAULT_GROUP_ID: "g:2a9f0874-bf99-4636-9376-0a34e5c09029",
     APP_ID: "bfa34a1d-fc8a-40f3-8a52-200f20cb236e",
     APP_SECRET: "c177f6fd-aa32-47a6-b1f7-d365421bd765",
     BOT_GUID: "Bwbj22wsmbj26e7f",
-    HOOK_AUTH_TOKEN: "flock_1636853250-1802416698"
+    BOT_TOKEN: "e0527c40-3c61-42ea-800a-6b827fffa2b4",
+    HOOK_AUTH_TOKEN: "flock_1636853250-1802416698",
+    FLOCK_SEND_MESSAGE_ENDPOINT: "https://api.flock.co/v1/chat.sendMessage"
 };
 
 module.exports = config;
---
+++ services/botService.js
@@ -0,0 +1,29 @@
+var Message = require("../models/message");
+var config = require("../config/appconfig");
+var request = require("request");
+function BotService() {
+    var pushMessageTemplate = [ "Hey @USERNAME, I received your changes",
+         "Hurray USERNAME, I got you"
+        ];
+    var options = {
+        "url" : config.FLOCK_SEND_MESSAGE_ENDPOINT,
+        "X-Flock-User-Token" : config.BOT_TOKEN
+    }
+
+    this.sendPushNotification = function (pushConfig) {
+        var message = new Message();
+        message.to(config.DEFAULT_GROUP_ID)
+            .from(config.BOT_GUID)
+            .text(pushMessageTemplate[0].replace("USERNAME", pushConfig.adName));
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            }
+        }
+        request(options, callback);
+    }
+
+}
+
+module.exports = BotService;
