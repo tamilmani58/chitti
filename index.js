0b90f7426ea9bba240444812262fbe3c254e503d
tamil3758 <tamil.3758@gmail.com>
2016-09-24 15:09:52 +0530

    support for flock node sdk

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
  * Created by tamil on 24/9/16.
  */
 var express = require('express');
+var flock = require('flockos');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(flock.validationTokenChecker);
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
@@ -15,6 +18,14 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
     console.log(req.body);
     res.send("done");
+});
+
+app.post('/events', flock.router);
+
+flock.events.on('client.recieve', function (event) {
+    return {
+        text: 'Got: ' + event.text
+    }
 });
 
 app.listen(1432, function () {
