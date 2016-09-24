72b3e15408927f2932223c5333bdef90321d7313
tamil3758 <tamil.3758@gmail.com>
2016-09-24 14:11:04 +0530

    support post

--- index.js
+++ index.js
@@ -4,8 +4,17 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
+});
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+    res.send("done");
 });
 
 app.listen(1432, function () {
